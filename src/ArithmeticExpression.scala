56f5b9b81e8aa448b0b335818619747036591811
jcrucifix <jonathan.crucifix@gmail.com>
2015-07-19 17:11:10 +0200

    Initial commit

---
+++ src/ArithmeticExpression.scala
@@ -0,0 +1,11 @@
+/**
+ * Created by Jonathan on 19-07-15.
+ */
+object ArithmeticExpression {
+
+  def main(args: Array[String]) {
+    val x = 5
+
+    println((1).+(((2).*(3))./(x)))
+  }
+}
---
+++ src/HelloWord.scala
@@ -0,0 +1,8 @@
+/**
+ * Created by Jonathan on 18-07-15.
+ */
+object HelloWord {
+  def main(args: Array[String]) {
+    println("Hello, world!")
+  }
+}
